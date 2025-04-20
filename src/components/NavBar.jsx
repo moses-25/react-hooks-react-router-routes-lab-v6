79a87a0fdd5a8dc900bdb97a64b19ee6272acfdf
moses-25 <moses.ochieng1@student.moringaschool.com>
2025-04-20 14:03:20 +0300

    fix: update NavBar component structure and restore navigation links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,14 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
-    </nav>
+    <div className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
     );
-};
+}
 
 export default NavBar;
